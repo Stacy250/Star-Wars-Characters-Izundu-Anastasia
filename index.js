d16287d1cba133f5829bfe8d7eae2cbe90ce7ee3
Stacy250 <ogochukwuizundu23@gmail.com>
2021-10-16 17:50:51 +0100

    firstadd

---
+++ index.js
@@ -0,0 +1,12 @@
+// const API_URL = "https://swapi.dev/api/people";
+// const randomStarWars = document.querySelector(.random-star);
+
+// async function getStarWars(){
+//  fetch(API_URL);
+//     .then(res => res.json())
+//     .then(data =>)
+// }
+
+// getStarWars();
+
+// // module.exports = {main}
